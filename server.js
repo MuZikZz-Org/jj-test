8461c021a9373264cc52e3d55ef56e8f4af9bf5f
natthidakl <140678535+natthidakl@users.noreply.github.com>
2023-10-10 03:48:58 +0000

    Update server.js

--- server.js
+++ server.js
@@ -18,4 +18,4 @@ http.createServer(function(req,res){
         res.end('Invalid Request!');
     // res.writeHead(200, {'Content-Type': 'text/html'});
     // res.end('Hello NodeJS from Muzikzz Org');
-}).listen(1234, () => console.log('Server running on localhost:1234...')) ;
+}).listen(3000, () => console.log('Server running on localhost:3000...')) ;
