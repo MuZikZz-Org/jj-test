adb86d0d315a365a7b170cbc45c52a802b8dce97
natthidakl <140678535+natthidakl@users.noreply.github.com>
2023-10-12 09:47:44 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
-let http = require('http');
+const http = require('http');
 
-http.createServer(function(req,res){
-    if (req.url == '/') { //check the URL of the current request
-        // set response header
-        res.writeHead(200, { 'Content-Type': 'text/html' }); 
-        // set response content    
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write('<html><body><p>This is home Page.</p></body></html>');
         res.end();
-    }
-    else if (req.url == "/healthz") {
-        // Handle readiness and liveness probe request
+    } else if (req.url === '/healthz') {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('OK');
-        return;
-    }
-    else
+    } else {
         res.end('Invalid Request!');
-    // res.writeHead(200, {'Content-Type': 'text/html'});
-    // res.end('Hello NodeJS from Muzikzz Org');
-}).listen(3000, () => console.log('Server running on localhost:3000...')) ;
+    }
+});
+
+server.listen(3000, '0.0.0.0', () => {
+    console.log('Server running on 0.0.0.0:3000...');
+});
