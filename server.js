12648790b4ecac48bfa0d9c44fe7c7cfba0a144e
natthidakl <140678535+natthidakl@users.noreply.github.com>
2023-10-13 10:30:26 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
+    if (req.url === '/home') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write('<html><body><p>This is home Page.</p></body></html>');
         res.end();
