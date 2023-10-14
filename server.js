e1a33e69cbea4673022de293303939fe0bbbceee
natthidakl <140678535+natthidakl@users.noreply.github.com>
2023-10-14 06:21:03 +0000

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(3000, () => {
-    console.log('Server running on Port 3000...');
+    console.log('Server running on port 3000...');
 });
 
