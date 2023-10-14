6d625867f037bc27e37e1c38a3aded3adfc3d8f0
natthidakl <140678535+natthidakl@users.noreply.github.com>
2023-10-14 06:00:17 +0000

    Update server.js

--- server.js
+++ server.js
@@ -6,14 +6,17 @@ const server = http.createServer((req, res) => {
         res.write('<html><body><p>This is home Page.</p></body></html>');
         res.end();
     } else if (req.url === '/healthz') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('OK');
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>Healthy</p></body></html>');
+        res.end();
+        // res.writeHead(200, { 'Content-Type': 'text/plain' });
+        // res.end('OK');
     } else {
         res.end('Invalid Request!');
     }
 });
 
 server.listen(3000, () => {
-    console.log('Server running on port 3000...');
+    console.log('Server running on Port 3000...');
 });
 
